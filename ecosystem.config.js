cf309ca3ca9017eb7c980816f012ea376c951d06
root <root@docker>
2022-03-14 17:35:15 -0300

    Docker - Testing PM2

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,33 +1,30 @@
 module.exports = {
-    "apps": [
-        {
-            "name": "Bash",
-            "script": "run",
-            "interpreter": "/bin/bash",
-            "instances": -1,
-            "autorestart": true,
-            "watch": true,
-            "max_memory_restart": "100M"
-        }
-    ],
+    apps: [{
+        name: 'Bash',
+        script: 'run',
+        interpreter: '/bin/bash',
+        autorestart: 'true',
+        watch: 'true'
+    }],
 
-    "deploy": {
-        "docker": {
-            "key": "~/.ssh/id_rsa",
-            "user": "linuxserver.io",
-            "host": [
+    deploy: {
+        docker: {
+            user: 'linuxserver.io',
+            host: [
                 {
-                    "host": "172.17.0.2",
-                    "port": "2222"
+                    'host': '172.17.0.2',
+                    'port': '2222'
                 }
             ],
-            "ref": "origin/master",
-            "repo": "https://github.com/kaissi/pm2-teste.git",
-            "path": "/workspace",
-            "post-deploy": " \
+            ref: 'origin/master',
+            repo: 'https://github.com/kaissi/pm2-teste.git',
+            path: '/workspace',
+            'pre-deploy-local': 'hostname',
+            'post-deploy': ' \
                 . ${HOME}/.bashrc \
                     && pm2 reload ecosystem.config.js --env docker \
-            "
+            ',
+            'pre-setup': 'hostname'
         }
     }
 }
