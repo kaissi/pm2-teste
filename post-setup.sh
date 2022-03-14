fb910aaec7d489d247b0d3bd043b17c28322aca0
root <root@docker>
2022-03-14 19:39:15 -0300

    Docker - Testing PM2

--- post-setup.sh
+++ post-setup.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 echo "[post-setup] hostname=$(hostname)" \
+    && . ${HOME}/.bashrc \
     && pm2 start ecosystem.config.js --env docker
 
--- pre-setup.sh
+++ pre-setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "[pre-setup] hostname=$(hostname)"
-$PWD
+
 if [ -d /workspace/source ]; then
     rm -rf /workspace/source
 fi
