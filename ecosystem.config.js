82650798ae7f30decdbf77a12ae3b3b2cc24c618
root <root@docker>
2022-03-14 19:09:29 -0300

    Docker - Testing PM2

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -23,16 +23,16 @@ module.exports = {
             repo: 'https://github.com/kaissi/pm2-teste.git',
             path: '/workspace',
             'pre-setup': " \
-                echo '[pre-setup] hostname=$(hostname)' \
+                echo '[pre-setup] hostname=\$(hostname)' \
             ",
             'post-setup': " \
-                echo '[post-setup] hostname=$(hostname)' \
+                echo '[post-setup] hostname=\$(hostname)' \
             ",
             'pre-deploy-local': " \
-                echo '[pre-deploy-local] hostname=$(hostname)' \
+                echo '[pre-deploy-local] hostname=\$(hostname)' \
             ",
             'post-deploy': " \
-                echo '[post-deploy] hostname=$(hostname)' \
+                echo '[post-deploy] hostname=\$(hostname)' \
                     && . ${HOME}/.bashrc \
                     && pm2 reload ecosystem.config.js --env docker \
             "
