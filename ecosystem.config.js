d9bf09179a09e8980c85ff2b64b57c58844eefe6
root <root@docker>
2022-03-14 08:37:14 -0300

    Docker - Testing PM2

---
+++ ecosystem.config.js
@@ -0,0 +1,28 @@
+module.exports = {
+    "apps": [
+        {
+            "name": "Bash",
+            "script": "run",
+            "interpreter": "/bin/bash",
+            "instances": -1,
+            "autorestart": true,
+            "watch": true,
+            "max_memory_restart": "100M"
+        }
+    ],
+
+    "deploy": {
+        "docker": {
+            "key": "~/.ssh/id_rsa",
+            "user": "linuxserver.io",
+            "host": {
+                "host": "172.17.0.2",
+                "port": 2222
+            },
+            "ref": "origin/master",
+            "repo": "https://github.com/kaissi/pm2-teste.git",
+            "path": "/workspace",
+            "post-deploy": "ls -laht"
+        }
+    }
+}
